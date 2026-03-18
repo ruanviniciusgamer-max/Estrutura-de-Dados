136e2d9e91b7135a30917afcbdc71d2f90661bab
RUAN VINICIUS ESTEVAM MELO <ruan.melo007@cs.cruzeirodosul.edu.br>
2026-03-18 10:19:19 -0300

    sla

--- buscas.java
+++ buscas.java
@@ -5,6 +5,11 @@ public class buscas {
         double[] vetorDesordenado = {4.3, 6.1, 0.1, 1.9, 2.4, 7.2, 5.5};
         int pos = buscaSequencial(vetorDesordenado, 2.4);
         System.out.println("Localizado na posição (indice): " + pos);
+
+        //Busca binária
+        double[] vetorOrdenado = {1, 2, 4, 8, 10, 11, 12};
+        pos = buscaBinaria(vetorOrdenado, 1);
+        System.out.println("Localizado no vetor ordenado na posição: " + pos);
     }
 
     public static int buscaSequencial(double[] vetor, double valorPesquisado){
@@ -14,6 +19,25 @@ public class buscas {
             }
         }
         return -1;
+        
     }
     
+    public static int buscaBinaria(double[] vetor, double valorPesquisado){
+        int inicio = 0;
+        int fim = vetor.length - 1;
+
+        while (inicio <= fim){
+            int meio = (inicio + fim) / 2;
+            if (vetor[meio] == valorPesquisado){
+                return meio;
+            }
+
+            if (valorPesquisado < vetor[meio]){
+                fim = meio - 1;
+            }else{
+                inicio = meio + 1;
+            }
+        }
+        return -1;
+    }
 }
