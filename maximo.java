f48695448823e2edb72cae5a60e3aafa0911ce18
RUAN VINICIUS ESTEVAM MELO <ruan.melo007@cs.cruzeirodosul.edu.br>
2026-04-08 09:54:30 -0300

    maximo

--- maximo.java
+++ maximo.java
@@ -7,6 +7,8 @@ public class maximo {
     public maximo() {
         int a[] = {12, 11, 21, 89, 99, 45, 89, 12, 24, 6, 70, 1002, 999, 987};
         System.out.println("Maior dos valores de 1: " + maximoIterativo(a));
+        System.out.println("Maior dos valores 2: " + maximoRecursivo(a, 0, a.length - 1, 0));
+        System.out.println("Maior dos valores 3: " + maximoRecursivo(a, 0, 3, 0));
     }
 
     public int maximoRecursivo(int vet[], int inicio, int fim, int nivel) {
